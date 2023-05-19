75b84aa77c45da0415b9a93c0eb5dea8477054ee
Manas Rai <106554051+manasrai1406@users.noreply.github.com>
2023-05-20 01:56:46 +0530

    Add files via upload
    improving the regression model

--- regression_with_tensorflow.py
+++ regression_with_tensorflow.py
@@ -49,5 +49,171 @@ model.fit(tf.expand_dims(x, axis=-1), y, epochs=5)
 
 model.predict([17.0])
 
+"""# Improving Our Model
 
+##We can improve our model by following methods:
+1. **creating a model** -> here we can add more layers,increase the number of hidden units,change activation function of each layer.
+2. **Compiling a model** -> here we might change the optimization function or perhaps **Learning Rate** of optimization function.
+3. **Fitting a Model** -> here we might fit model for more **epochs** or more data.
+"""
+
+tf.random.set_seed(42)
+#1
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(1)
+])
+
+#2
+model.compile(loss=tf.keras.losses.mae,
+              optimizer=tf.keras.optimizers.SGD(),
+              metrics = ["mae"]
+              )
+
+#3
+model.fit(tf.expand_dims(x, axis=-1), y, epochs=100)
+
+model.predict([17.0])
+
+tf.random.set_seed(42)
+#1
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(100,activation= "relu"),
+    tf.keras.layers.Dense(1)
+])
+
+#2
+model.compile(loss=tf.keras.losses.mae,
+              optimizer=tf.keras.optimizers.SGD(),
+              metrics = ["mae"]
+              )
+
+#3
+model.fit(tf.expand_dims(x, axis=-1), y, epochs=100)
+
+model.predict([17.0])
+
+tf.random.set_seed(42)
+#1
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(50,activation = None),
+    tf.keras.layers.Dense(1)
+])
+
+#2
+model.compile(loss=tf.keras.losses.mae,
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics = ["mae"]
+              )
+
+#3
+model.fit(tf.expand_dims(x, axis=-1), y, epochs=100)
+
+model.predict([17.0])
+
+tf.random.set_seed(42)
+#1
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(50,activation = None),
+    tf.keras.layers.Dense(1)
+])
+
+#2
+model.compile(loss="mae",
+              optimizer=tf.keras.optimizers.Adam(lr=0.001),
+              metrics = ["mae"]
+              )
+
+#3
+model.fit(tf.expand_dims(x, axis=-1), y, epochs=100)
+
+model.predict([17.0])
+
+"""# Evaluating a model preformance"""
+
+X=tf.range(-100,100,4)
+X
+
+Y = X + 10
+Y
+
+import matplotlib.pyplot as plt
+plt.scatter(X,Y)
+
+len(X)
+
+#Splitting_the _data
+X_train=X[:40]
+Y_train=Y[:40]
+X_test = X[40:]
+Y_test = Y[40:]
+len(X_train),len(X_test),len(Y_train),len(Y_test)
+
+plt.figure(figsize=(7,7))
+plt.scatter(X_train,Y_train,c="b",label="training_data")
+
+plt.scatter(X_test,Y_test,c="g",label="testing data")
+plt.legend()
+
+tf.random.set_seed(42)
+#1
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(1)
+])
+
+#2
+model.compile(loss=tf.keras.losses.mae,
+              optimizer=tf.keras.optimizers.SGD(),
+              metrics = ["mae"]
+              )
+
+#3
+model.fit(tf.expand_dims(X_train, axis=-1), Y_train, epochs=100)
+
+model.summary()
+
+tf.random.set_seed(42)
+#1
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(3,input_shape = [1])
+])
+
+#2
+model.compile(loss=tf.keras.losses.mae,
+              optimizer=tf.keras.optimizers.SGD(),
+              metrics = ["mae"]
+              )
+
+model.summary()
+
+model.fit(tf.expand_dims(X_train, axis=-1), Y_train, epochs=100,verbose=0)
+
+from tensorflow.keras.utils import *
+plot_model(model=model,show_shapes = True)
+
+Y_pred =model.predict(X_test)
+Y_pred
+
+import matplotlib.pyplot as plt
+
+def plot_predictions(
+    train_data=X_train,
+    train_labels = Y_train,
+    test_data = X_test,
+    test_labels = Y_test,
+    predictions = Y_pred
+):
+ plt.figure(figsize=(10,7))
+
+ plt.scatter(train_data,train_labels,c="b",label="training data")
+
+
+ plt.scatter(test_data,test_labels,c="g",label="testing data")
+
+ plt.scatter(test_data,predictions,c="r",labels = "Predictions")
+
+ plt.legend();
+
+ plt.show()
+
+plot_predictions()
 
